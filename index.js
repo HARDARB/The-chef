ec116d943bbba3059ac97c40d6f4e46175a0ad73
Adeyinka Adebayo A <adeyinkaadebayo032@gmail.com>
2023-12-03 15:44:42 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+  // Scroll reveal for the whole body
+  ScrollReveal({
+    reset: true,
+    distance: "150px",
+    duration: 2500,
+    delay: 400
+  });
+  
+
+  ScrollReveal().reveal('.stagger',{delay: 300, origin: 'bottom', interval: 500});
+
+
+  ScrollReveal().reveal('.from-right',{delay: 300, origin: 'right', });
+  ScrollReveal().reveal('.from-left ',{delay: 400, origin: 'left',});
+  ScrollReveal().reveal('.from-bottom ',{delay: 300, origin: 'bottom',});
+  ScrollReveal().reveal('.from-top',{delay: 400, origin: 'top',});
+
+  ScrollReveal().reveal('.graphics-text',{delay: 300, origin: 'right', });
+  ScrollReveal().reveal('.graphicsstagger',{delay: 400, origin: 'left',});
+  
